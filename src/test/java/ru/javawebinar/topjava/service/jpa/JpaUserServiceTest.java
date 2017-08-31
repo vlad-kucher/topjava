61458192a2053970ee613c839ad3fbbace9f3e79
Vlad <vlad.kucher.92@gmail.com>
2017-08-31 18:45:29 +0300

    HW6

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -1,10 +1,24 @@
 package ru.javawebinar.topjava.service.jpa;
 
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
 
 @ActiveProfiles(JPA)
 public class JpaUserServiceTest extends AbstractUserServiceTest {
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    public void setUp() throws Exception {
+        if (!environment.acceptsProfiles(Profiles.JDBC)) {
+            service.evictCache();
+            jpaUtil.clear2ndLevelHibernateCache();
+        }
+    }
 }
