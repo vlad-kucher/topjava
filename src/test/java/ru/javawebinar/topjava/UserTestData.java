ab4f08e1a1b72fec4b73294306c81966d2bbef16
Vlad <vlad.kucher.92@gmail.com>
2017-09-02 15:09:50 +0300

    test with matcher

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -15,7 +15,7 @@ public class UserTestData {
     public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.ROLE_USER);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN, Role.ROLE_USER);
 
-    public static final BeanMatcher<User> MATCHER = new BeanMatcher<>(
+    public static final BeanMatcher<User> MATCHER = BeanMatcher.of(User.class,
             (expected, actual) -> expected == actual ||
                     (Objects.equals(expected.getPassword(), actual.getPassword())
                             && Objects.equals(expected.getId(), actual.getId())
--- src/test/java/ru/javawebinar/topjava/matcher/BeanMatcher.java
+++ src/test/java/ru/javawebinar/topjava/matcher/BeanMatcher.java
@@ -1,9 +1,17 @@
 package ru.javawebinar.topjava.matcher;
 
 import org.junit.Assert;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import ru.javawebinar.topjava.TestUtil;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 /**
  * This test Matcher assert equality of beans and collections
@@ -18,16 +26,24 @@ public class BeanMatcher<T> {
 
     private Equality<T> equality;
 
+    private Class<T> entityClass;
+
     public interface Equality<T> {
         boolean areEqual(T expected, T actual);
     }
 
-    public BeanMatcher() {
-        this((T expected, T actual) -> expected == actual || String.valueOf(expected).equals(String.valueOf(actual)));
+    private BeanMatcher(Class<T> entityClass, Equality<T> equality) {
+        this.entityClass = entityClass;
+        this.equality = equality;
     }
 
-    public BeanMatcher(Equality<T> equality) {
-        this.equality = equality;
+    public static <T> BeanMatcher<T> of(Class<T> entityClass) {
+        return of(entityClass,
+                (T expected, T actual) -> expected == actual || String.valueOf(expected).equals(String.valueOf(actual)));
+    }
+
+    public static <T> BeanMatcher<T> of(Class<T> entityClass, Equality<T> equality) {
+        return new BeanMatcher<>(entityClass, equality);
     }
 
     private class Wrapper {
@@ -51,6 +67,18 @@ public class BeanMatcher<T> {
         }
     }
 
+    private T fromJsonValue(String json) {
+        return JsonUtil.readValue(json, entityClass);
+    }
+
+    private List<T> fromJsonValues(String json) {
+        return JsonUtil.readValues(json, entityClass);
+    }
+
+    public T fromJsonAction(ResultActions action) throws UnsupportedEncodingException {
+        return fromJsonValue(TestUtil.getContent(action));
+    }
+
     public void assertEquals(T expected, T actual) {
         Assert.assertEquals(wrap(expected), wrap(actual));
     }
@@ -65,5 +93,34 @@ public class BeanMatcher<T> {
 
     private List<Wrapper> wrap(List<T> list) {
         return list.stream().map(this::wrap).collect(Collectors.toList());
+    }
+
+    public ResultMatcher contentMatcher(T expect) {
+        return content().string(
+                new TestMatcher<T>(expect) {
+                    @Override
+                    protected boolean compare(T expected, String body) {
+                        Wrapper expectedForCompare = wrap(expected);
+                        Wrapper actualForCompare = wrap(fromJsonValue(body));
+                        return expectedForCompare.equals(actualForCompare);
+                    }
+                });
+    }
+
+    @SafeVarargs
+    public final ResultMatcher contentListMatcher(T... expected) {
+        return contentListMatcher(Arrays.asList(expected));
+    }
+
+    public final ResultMatcher contentListMatcher(List<T> expected) {
+        return content().string(
+                new TestMatcher<List<T>>(expected) {
+                    @Override
+                    protected boolean compare(List<T> expected, String actual) {
+                        List<Wrapper> expectedList = wrap(expected);
+                        List<Wrapper> actualList = wrap(fromJsonValues(actual));
+                        return expectedList.equals(actualList);
+                    }
+                });
     }
 }
