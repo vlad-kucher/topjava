f8aed1ed9c0c69132a0ec27ee618f7e74d419e8e
Vlad <vlad.kucher.92@gmail.com>
2017-09-19 12:12:52 +0300

    js i18n

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -11,11 +11,13 @@ function makeEditable() {
 }
 
 function add() {
+    $("#modalTitle").html(i18n["addTitle"]);
     form.find(":input").val("");
     $("#editRow").modal();
 }
 
 function updateRow(id) {
+    $("#modalTitle").html(i18n["editTitle"]);
     $.get(ajaxUrl + id, function (data) {
         $.each(data, function (key, value) {
             form.find("input[name='" + key + "']").val(value);
@@ -30,7 +32,7 @@ function deleteRow(id) {
         type: "DELETE",
         success: function () {
             updateTable();
-            successNoty("Deleted");
+            successNoty("common.deleted");
         }
     });
 }
@@ -47,7 +49,7 @@ function save() {
         success: function () {
             $("#editRow").modal("hide");
             updateTable();
-            successNoty("Saved");
+            successNoty("common.saved");
         }
     });
 }
@@ -61,10 +63,10 @@ function closeNoty() {
     }
 }
 
-function successNoty(text) {
+function successNoty(key) {
     closeNoty();
     new Noty({
-        text: "<span class='glyphicon glyphicon-ok'></span> &nbsp;" + text,
+        text: "<span class='glyphicon glyphicon-ok'></span> &nbsp;" + i18n[key],
         type: 'success',
         layout: "bottomRight",
         timeout: 1000
@@ -74,7 +76,7 @@ function successNoty(text) {
 function failNoty(event, jqXHR, options, jsExc) {
     closeNoty();
     failedNote = new Noty({
-        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;Error status: " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
+        text: "<span class='glyphicon glyphicon-exclamation-sign'></span> &nbsp;" + i18n["common.errorStatus"] + ": " + jqXHR.status + (jqXHR.responseJSON ? "<br>" + jqXHR.responseJSON : ""),
         type: "error",
         layout: "bottomRight"
     }).show();
