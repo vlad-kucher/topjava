ef960c8681facce4ce981dcaee2e557336bdcd73
Vlad <vlad.kucher.92@gmail.com>
2017-10-03 22:24:58 +0300

    fix

--- hr.bat
+++
@@ -1,2 +0,0 @@
-call mvn -B -s settings.xml -DskipTests=true clean package
-call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
